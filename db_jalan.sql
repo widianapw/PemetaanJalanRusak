251941df7b9597a8bbc7511eb36d04303e2f899b
widianapw <widianaputraa@gmail.com>
2020-03-17 10:40:15 +0800

    env

--- db_jalan.sql
+++ db_jalan.sql
@@ -9,6 +9,7 @@ MySQL - 10.4.11-MariaDB : Database - db_pemetaan_jalan_rusak
 /*!40101 SET SQL_MODE=''*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 /*Table structure for table `tb_broken_road` */
@@ -142,5 +143,6 @@ insert  into `users`(`id`,`name`,`username`,`email`,`password`,`user_role`,`stat
 (2,'user',NULL,'user@me.com','$2y$10$55dEpf3iNgbgYkYYp3nz2utEr1dHAMp/dvqKRKoT10Mqno3s/6cGS','user','0','2020-03-16 07:40:12','2020-03-16 07:40:12');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
